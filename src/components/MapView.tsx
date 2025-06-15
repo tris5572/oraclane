8bf4c42c2e04ca51647c924eda3a82a995b7c659
tris <nexus4fortune@gmail.com>
2025-06-15 10:14:42 +0900

    feat: 合流地点のデータを元にした表示を実装

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -6,8 +6,29 @@ import {
   ScaleControl,
 } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { PinDecrease } from "./PinDecrease";
+import { useMemo } from "react";
+import { DECREASE_DATA } from "../assets/mergeData";
 
 export function MapView() {
+  const decreaseMarkers = useMemo(
+    () =>
+      DECREASE_DATA.map((data) => (
+        <ReactMap.Marker
+          key={`marker-${data.longitude}-${data.latitude}`}
+          longitude={data.longitude}
+          latitude={data.latitude}
+          anchor="center"
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          <PinDecrease data={data} />
+        </ReactMap.Marker>
+      )),
+    [],
+  );
+
   return (
     <ReactMap.Map
       initialViewState={{
@@ -17,6 +38,7 @@ export function MapView() {
       }}
       mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
     >
+      {decreaseMarkers}
       <ScaleControl />
       <NavigationControl />
       <FullscreenControl />
