925c39c72c86bfb037475294f92e516e155d98bd
tris <nexus4fortune@gmail.com>
2025-06-19 05:26:46 +0900

    feat: 情報表示ビューを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { MapView } from "./components/MapView";
 
 export function App() {
   return (
-    <div className="map-view">
-      <MapView />
+    <div className="app-wrapper">
+      <div className="map-view">
+        <MapView />
+      </div>
+      <div className="information-view">情報</div>
     </div>
   );
 }
